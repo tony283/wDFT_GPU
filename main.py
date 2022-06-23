0ee27760fd852bf25973a32e8181920efe3f49c2
tony283 <84143132+tony283@users.noreply.github.com>
2022-06-23 11:15:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+#Copyright Lin RF
+
 import cupy as cp
 import numpy as np
 
-a = cp.array([[1,2,3],[2,3,4],[3,3,1]])
-print(a)
-b = a@a
-#print(b)
+global basis
+global method
+
+def ReadFile():
+    return 0
+
+
