8970f82c89f680938c962bb3826d8ec298b2ae9b
tony283 <84143132+tony283@users.noreply.github.com>
2022-06-23 12:38:31 +0800

    Update

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-#Copyright Lin RF
-
+# Copyright Lin RF
+# wDFT is a GPU-accelerated DFT program running on windows.
+# The aim of wDFT is to fully utilize GPU to make commercial computer possible to calculate DFT on windows.
+# The advantage is that the computer only needs a good GPU and the calculation is totally free. 
+# Also, we aim to simplize the manipulation on DFT to make it easy to use.
 import cupy as cp
 import numpy as np
-
-global basis
-global method
-
-def ReadFile():
-    return 0
+import wDFT_Process_Func as wF
 
 
+# Basic selection of basis, task and functional
+_Settings = dict()
+_Settings['basis'] = 'a'
+_Settings['functional'] = 'a'
+_Settings['task'] = 'sp'
+_Settings['rotation'] = 1
+_Settings['charge'] = 0
+_Settings['coord'] = cp.zeros((1,1))
+_Settings['element'] = []
+
+
+wF.ReadFile(_Settings)
+wF.EnergyCalculate(_Settings)
---
+++ wDFT_Process_Func.py
@@ -0,0 +1,6 @@
+
+
+def ReadFile(_Settings:dict):
+    
+    _Settings['basis'] = 'aaa'
+    return 0
